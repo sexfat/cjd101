9ffc3b7c15c723044300bf6a9ee9149a56e6417d
sexfat <bryant6831@gmail.com>
2025-05-27 11:03:38 +0800

    add 99乘法表

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+const tableDiv = document.getElementById('table');
+let html = '<table>';
+for (let i = 1; i <= 9; i++) {
+    html += '<tr>';
+    for (let j = 1; j <= 9; j++) {
+        html += `<td>${i}x${j}=${i * j}</td>`;
+    }
+    html += '</tr>';
+}
+html += '</table>';
+tableDiv.innerHTML = html;
